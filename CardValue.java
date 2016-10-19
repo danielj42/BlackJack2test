c183ced8de0844d324b8b31e9639fca7811dedfe
Jimmy-Cannon <arinze.nwadinkpa@gmail.com>
2016-10-19 20:36:47 +0200

    lite ändringar

--- CardValue.java
+++ CardValue.java
@@ -1,5 +1,6 @@
 package team2;
 
+//enum för kortens värde
 public enum CardValue {
 	TWO(2),
 	THREE(3),
@@ -14,13 +15,15 @@ public enum CardValue {
 	QUEEN(12),
 	KING(13),
 	ACE(14);
+
+	//instansvariabel
+	private int cardValue;
 	
-	private int CardValue;
-	
+	//konstruktor
 	private CardValue(int cardvalue){
 		this.CardValue = cardvalue;
 	}
-	
+	//getter
 	private int getCardValue(){
 		return CardValue;
 	}
