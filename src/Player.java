72f0a68f6e7bf4bd36fca7cb61091bdd79fc5e73
parvism@gmail.com <parvism@gmail.com>
2016-10-20 19:41:00 +0200

    some code added in Class player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,35 @@
-
+/* Player.java
+ * Spelare klassen för programmet Blackjack2
+ * Java 16, Kursen agila metoder
+ * Parviz Mozaffari
+ */
+ 
 public class Player {
+	
+	private String name;
+	private int credit;
+	
+	public Player(){
+		name = "Unknown";
+		credit = 0;
+	}
+	
+	public Player(String aName, int someCredit){
+		name = aName;
+		credit = someCredit;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getCredit(){
+		return credit;
+	}
+	
+	public void updateCredit(int aCredit){
+		credit += nCredit;
+	}
+	
 
 }
