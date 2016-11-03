21cced7d1c5c5e2e15e220dce1a71d34595bf424
John Claeson <john.claeson@gmail.com>
2016-11-03 10:56:11 +0100

    la till metoder i deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,42 +1,47 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
  
 public class Deck
 {
-  private ArrayList<Card> deck;	//lista för deck
- 
+  private ArrayList <Card> deck;
+  
   public Deck ()
   {
-    this.deck = new ArrayList<Card>();
-    for (int i=0; i<13; i++)	//13 kort
-    {
-      Face face = Face.values()[i];
- 
-      for (int j=0; j<4; j++)	//4 färger
-      {
-        Card card = new Card(face, Suit.values()[j]);
-        this.deck.add(card);
-      }
-    }
- 
-    Collections.shuffle(deck);
- 
-    //För Testa utskrift
-//    Iterator<Card> cardIterator = deck.iterator();
-//    while (cardIterator.hasNext())
-//    {
-//      Card aCard = (Card) cardIterator.next();
-//      //System.out.println(aCard.getFaceName() + " of " + aCard.getSuit());
-//    }
-//    
     
-    
+	fillDeck();
+//  getCard();
+//  System.out.println(getCard().getCardValue() +" "+ getCard().getSuit());
   }
   
-//  public void getCard(){
-//		 int i = (int)Math.random()*(deck.size());
-//		 System.out.println("Kortet blev: "+deck.get(i));
-//	  }
+  public Card getCard(){
+	  	
+		Card aCard = deck.get(0); 
+		deck.remove(0);
+		return aCard;
+	  	
+	  }
+  
+  public void fillDeck(){
+	  
+	  this.deck = new ArrayList<Card>();
+	    for (int i=0; i<13; i++)
+	    {
+	      Face face = Face.values()[i];
+	 
+	      for (int j=0; j<4; j++)
+	      {
+	        Card card = new Card(face, Suit.values()[j]);
+	        this.deck.add(card);
+	      }
+	    }
+	 
+	    Collections.shuffle(deck);
+  }
+  
+  public void Reset(){
+	  deck.clear();
+	  fillDeck();
+	  
+  }
 }
